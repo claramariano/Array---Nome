c22d99346b8fbe133b8074ef3cd5465107fd0d7a
claramariano <claraamariano@gmail.com>
2025-04-07 09:24:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var nomes = ["Pedro", "João", "Rafael", "Arthur", "Davi", "Theo", "Benjamin", "Enzo", "Lucas", "Gabriel",
+ "Alice", "Helena", "Laura", "Manuela", "Valentina", "Sofia", "Cecília", "Beatriz", "Júlia", "Yasmin",
+ "Miguel", "Gael", "Matheus", "Levi", "Noah", "Lucca", "Bernardo", "Samuel", "Daniel", "Heitor", "Clara"]
+
+var nomeEscolhido = parseInt(prompt("Digite seu número de identificação  de 0 a 30 "))
+
+
+document.write ( "O número escolhido pertence a  "  + nomes[nomeEscolhido])
+   
